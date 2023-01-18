a336b57e20a52799a2f9e8ecab3cd765d9e25480
saksham phul <phulsaksham2@gmail.com>
2023-01-18 09:57:07 -0500

    Improve code after PR feedback

--- scripts/format_parser.py
+++ scripts/format_parser.py
@@ -6,76 +6,68 @@ import argparse
 
 
 def extract_format_fields_vcf(lines, header):
-    ''' Returns gene for single variant call by parsing format field from vcf
-        Args:
-            Lines : format field from vcf file
-            header: CSQ header from vcf file
-        Return:
-            Returns Gene name (a string) for single variant call from the format field
-    '''
+    """Returns gene for single variant call by parsing format field from vcf
+    Args:
+        Lines : format field from vcf file
+        header: CSQ header from vcf file
+    Return:
+        Returns Gene name (a string) for single variant call from the format field
+    """
     format_split = []
     format_seg = lines.split(",")
     for seg in format_seg:
         format_split.append(seg.split("|"))
     Fields = header.split("|")
     label = "Gene"
-    gene = ''
+    gene = ""
     if label in Fields:
         index_to_pick = Fields.index(label)
         for split in format_split:
-            if split[Fields.index('PICK')] == '1': #Get gene if pick field in the format is 1
-                gene = split[index_to_pick-1]
+            # Get gene if pick field in the format is 1
+            if split[Fields.index("PICK")] == "1":
+                gene = split[index_to_pick - 1]
                 break
-            elif split[Fields.index('CANONICAL')] == 'YES': #Get gene if canonical field in the format is YES
-                gene = split[index_to_pick-1]
+            # Get gene if canonical field in the format is YES
+            elif split[Fields.index("CANONICAL")] == "YES":
+                gene = split[index_to_pick - 1]
                 break
-            elif split[Fields.index('SYMBOL')] == 'MODIFIER': #else select gene that is next to modifer field 
-                modifier = Fields.index('MODIFIER')
-                gene = split[modifier+1]
+            # Get gene if TSL field in the format is 1
+            elif split[Fields.index("TSL")] == "1":
+                gene = split[index_to_pick - 1]
+                break
+            # Get gene if BIOTYPE field in the format is protein_coding
+            elif split[Fields.index("BIOTYPE")] == "protein_coding":
+                gene = split[index_to_pick - 1]
+                break
+            # else select gene that is next to modifer field
+            elif split[Fields.index("SYMBOL")] == "MODIFIER":
+                modifier = Fields.index("MODIFIER")
+                gene = split[modifier + 1]
     return gene
 
 
 def read_vcf_gene_list(file):
-    ''' Returns list of gene for variants called within the vcf file
-        Args:
-            file : VCF file in gz format
-        Return:
-            list of gene extracted from format field within a vcf file
-    '''
-    '''
-    if not (file.endswith('gz')):
-        raise IOError('vcf file required in gzip format')
-    with gzip.open(file, 'rb') as f:
-        Lines = f.readlines()
-
-    threshold_count = 10000000001  # very large number
-    data = []
+    """Returns list of gene for variants called within the vcf file
+    Args:
+        file : VCF file for a sample of interest
+    Return:
+        list of gene extracted from format field within a vcf file
+    """
     gene_list = []
-    for count, line in enumerate(Lines):
-        line_sep = re.split(r'\t+', line.decode('utf-8'))
-        sp = line_sep[0].split(",")
-        if '#CHROM' in line_sep:
-            threshold_count = count
-        elif '##INFO=<ID=CSQ' in sp:
-            CSQ_header = sp[3]
-        elif count > threshold_count:
-            gene_list.append(extract_format_fields_vcf(
-                line_sep[7], CSQ_header))
-            data.append(line_sep)
-    '''        
-    gene_list = []
-    with (gzip.open if file.endswith("gz") else open)(file, 'rt') as f:
+    with (gzip.open if file.endswith("gz") else open)(file, "rt") as f:
         # grab CSQ description string from header; contains field names separated by |
         for line in f:
-            if line.startswith('##INFO=<ID=CSQ'):
-                CSQ_header = line.split(',')[3]
+            if line.startswith("##INFO=<ID=CSQ"):
+                CSQ_header = line.split(",")[3]
                 break
-        # Reach the end of the header (lines that start with #) and process the first record 
+        # Reach the end of the header (lines that start with #) and process the first record
         for line in f:
-            if not line.startswith('#'):
-                gene_list.append(extract_format_fields_vcf(line.split('\t')[7], CSQ_header))
+            if not line.startswith("#"):
+                gene_list.append(
+                    extract_format_fields_vcf(line.split("\t")[7], CSQ_header)
+                )
                 break
         # Process remaining records
         for line in f:
-            gene_list.append(extract_format_fields_vcf(line.split('\t')[7], CSQ_header))    
-    return (gene_list)
+            gene_list.append(extract_format_fields_vcf(line.split("\t")[7], CSQ_header))
+    return gene_list
--- scripts/run_pysam.py
+++ scripts/run_pysam.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import argparse
-import subprocess
 import pandas as pd
 import pysam as pys
 import format_parser as ob
@@ -11,17 +10,15 @@ import format_parser as ob
 parser = argparse.ArgumentParser()
 
 # Adding optional argument
-parser.add_argument(
-    "-i", "--input", help="provide sample vcf file in gz format")
+parser.add_argument("-i", "--input", help="provide sample vcf file")
+
+parser.add_argument("-t", "--tsv", help="provide bcftools output in tsv format")
+
+parser.add_argument("-c", "--cram", help="provide cram/bam file for the sample")
 
 parser.add_argument(
-    "-t", "--tsv", help="provide bcftools output in tsv format")
-
-parser.add_argument(
-    "-c", "--cram", help="provide cram/bam file for the sample")
-
-parser.add_argument(
-    "-r", "--ref", help="provide human reference in fasta format")
+    "-r", "--ref", help="provide human reference in fasta format with index file"
+)
 
 parser.add_argument("-id", "--sampleid", help="provide sample ID")
 
@@ -32,51 +29,51 @@ args = parser.parse_args()
 
 
 def get_VAF_pos(cram_bam_path, chrom, pos, ref, alt, reference=None):
-    """ Return percent of reads match with alt and total number of reads found
-        Args:
-            cram_bam_path (str): path to CRAM or BAM file
-            chrom (str): locus chromosome name
-            pos (int): 1-based locus coordinate
-            reference: path to reference FASTA, required for CRAM
-                (default None)
-        Raises:
-            IOError if CRAM provided without reference
-                    if cram_path has neither cram nor bam extension
-        Return:
-            The percent of reads that match the given alt (as a float)
-            The total reads found at that position (int)
+    """Return percent of reads match with alt and total number of reads found
+    Args:
+        cram_bam_path (str): path to CRAM or BAM file
+        chrom (str): locus chromosome name
+        pos (int): 1-based locus coordinate
+        reference: path to reference FASTA, required for CRAM
+            (default None)
+    Raises:
+        IOError if CRAM provided without reference
+                if cram_path has neither cram nor bam extension
+    Return:
+        The percent of reads that match the given alt (as a float)
+        The total reads found at that position (int)
 
     """
     if len(ref) > 1 or len(alt) > 1:  # excluding indels
         return ("Indels are ignored at the moment", "Indels are ignored")
-    if cram_bam_path.endswith('cram'):
+    if cram_bam_path.endswith("cram"):
         if not reference:
-            raise IOError('Must provide reference with CRAM')
-        cram_bam = pys.AlignmentFile(
-            cram_bam_path, 'rc', reference_filename=reference)
-    elif cram_bam_path.endswith('bam'):
-        cram_bam = pys.AlignmentFile(cram_bam_path, 'rb')
+            raise IOError("Must provide reference with CRAM")
+        cram_bam = pys.AlignmentFile(cram_bam_path, "rc", reference_filename=reference)
+    elif cram_bam_path.endswith("bam"):
+        cram_bam = pys.AlignmentFile(cram_bam_path, "rb")
     else:
         raise IOError(
-            'File provided to "cram" argument must have .cram or .bam extension')
+            'File provided to "cram" argument must have .cram or .bam extension'
+        )
 
     mapq = []
     mq0 = 0
-    aligned_reads = cram_bam.fetch(chrom, pos-1, pos, multiple_iterators=False)
+    aligned_reads = cram_bam.fetch(chrom, pos - 1, pos, multiple_iterators=False)
     total_reads = 0
     count_required = 0
     for read in aligned_reads:
         start = read.pos  # read start position
-        diff = pos-start  # difference between start and pos
+        diff = pos - start  # difference between start and pos
         seq = read.query_sequence
         if len(seq) > diff:
-            if seq[diff-1] == alt:
-                count_required = count_required+1
+            if seq[diff - 1] == alt:
+                count_required = count_required + 1
             total_reads += 1
     if total_reads == 0:
         return ("No read found", "0")
     else:
-        return (round(count_required/total_reads, 4), total_reads)
+        return (round(count_required / total_reads, 4), total_reads)
 
 
 # extract gene list from vcf file
@@ -84,20 +81,29 @@ gene = ob.read_vcf_gene_list(args.input)
 del gene[-1]  # last element in the list is end of the file which is not required
 
 # read tsv file from bcftools
-dataframe = pd.read_csv(args.tsv, sep='\t')
+dataframe = pd.read_csv(args.tsv, sep="\t")
 
 # set up pandas dataframe
-dataframe.columns = ["popmax", "chr", "start", "stop", "ref",
-                     "alt", "ref,alt depth", "Germline_depth", "Germline_VAF"]
-dataframe['gene'] = gene
+dataframe.columns = [
+    "popmax",
+    "chr",
+    "start",
+    "stop",
+    "ref",
+    "alt",
+    "ref,alt depth",
+    "Germline_depth",
+    "Germline_VAF",
+]
+dataframe["gene"] = gene
 
 # remove entries with . as popmax
 dataframe = dataframe[dataframe.popmax != "."]
-dataframe['popmax'] = dataframe['popmax'].astype(float)
+dataframe["popmax"] = dataframe["popmax"].astype(float)
 
 # set criteria for rare variant
 dataframe = dataframe[dataframe.popmax < 0.01]
-dataframe = dataframe.drop(['popmax'], axis=1)  # not required anymore
+dataframe = dataframe.drop(["popmax"], axis=1)  # not required anymore
 
 list_VAF = []
 list_total_reads = []
@@ -106,7 +112,8 @@ list_total_reads = []
 for index, row in dataframe.iterrows():
     # print(row['chr'], row['start'])
     vaf, total_reads = get_VAF_pos(
-        args.cram, row['chr'], row['start'], row['ref'], row['alt'], args.ref)
+        args.cram, row["chr"], row["start"], row["ref"], row["alt"], args.ref
+    )
     list_VAF.append(vaf)
     list_total_reads.append(total_reads)
 dataframe["Tumor_VAF"] = list_VAF
@@ -114,16 +121,32 @@ dataframe["Tumor_depth"] = list_total_reads
 dataframe["BS_ID"] = args.sampleid
 
 # split columns
-dataframe[['ref_depth', 'alt_depth']] = dataframe['ref,alt depth'].str.split(
-    ',', 1, expand=True)
+dataframe[["ref_depth", "alt_depth"]] = dataframe["ref,alt depth"].str.split(
+    ",", 1, expand=True
+)
 
 # reorder the columns
-dataframe = dataframe[["BS_ID", "gene", "chr", "start", "stop", "ref", 'ref_depth', 'alt',
-                       'alt_depth', 'Germline_depth', 'Tumor_depth', 'Germline_VAF', 'Tumor_VAF']]
+dataframe = dataframe[
+    [
+        "BS_ID",
+        "gene",
+        "chr",
+        "start",
+        "stop",
+        "ref",
+        "ref_depth",
+        "alt",
+        "alt_depth",
+        "Germline_depth",
+        "Tumor_depth",
+        "Germline_VAF",
+        "Tumor_VAF",
+    ]
+]
 
 # output_file in tsv format
 output_file_name = args.output
-if not(output_file_name.endswith('tsv')):
-    output_file_name = output_file_name+".tsv"
+if not (output_file_name.endswith("tsv")):
+    output_file_name = output_file_name + ".tsv"
 
 dataframe.to_csv(output_file_name, sep="\t", index=False)
